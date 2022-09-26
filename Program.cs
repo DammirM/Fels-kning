282825d2256771538e8c4c558cbc9ee79409b110
Damir Mehic <damir_m87@hotmail.com>
2022-09-26 13:19:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Felsökning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.
+
+
+            //int number = 2;
+
+            //if (number > 3)
+            //{
+            //    Console.WriteLine("Talet är större än tre");
+            //}
+            //else if (number < 3)
+            //{
+            //    Console.WriteLine("Talet är mindre än tre");
+            //}
+
+            //2.
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //3.
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(j + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //4.
+
+            //int i = 1;
+            //while (i <= 5)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("hej");
+            //    }
+
+            //    Console.WriteLine();
+            //    i++;
+            //}
+
+            //5.
+
+            //int i = 1;
+
+            //switch (i)
+            //{
+            //    case 1:
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Two");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Other");
+            //        break;
+            //}
+
+            //6.
+
+            //int i = 10;
+
+            //if (i == 5)
+            //{
+            //    Console.WriteLine("i är 5");
+            //}
+
+            
+        }
+    }
+}
